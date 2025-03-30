bc25e259e69aa9f5f62a75d995fbe9f1da558e9f
Dan Davison <dan.davison@temporal.io>
2025-03-30 19:42:40 -0400

    Refactor: reduce LOC and lose some type safety

--- nexus/caller/app.py
+++ nexus/caller/app.py
@@ -1,5 +1,6 @@
 import asyncio
 import sys
+from typing import Any, Type
 
 from temporalio.client import Client
 from temporalio.worker import UnsandboxedWorkflowRunner, Worker
@@ -15,100 +16,20 @@ from nexus.caller.workflows import (
 interrupt_event = asyncio.Event()
 
 
-async def execute_echo_caller_workflow():
+async def execute_workflow(workflow_cls: Type[Any], input: Any) -> None:
     client = await Client.connect("localhost:7233", namespace="my-caller-namespace")
     task_queue = "my-caller-task-queue"
 
     async with Worker(
         client,
         task_queue=task_queue,
-        workflows=[EchoCallerWorkflow],
+        workflows=[workflow_cls],
         workflow_runner=UnsandboxedWorkflowRunner(),
     ):
         print("🟠 Caller worker started")
         result = await client.execute_workflow(
-            EchoCallerWorkflow.run,
-            "hello",
-            id="my-caller-workflow-id",
-            task_queue=task_queue,
-        )
-        print("🟢 workflow result:", result)
-
-
-async def execute_echo2_caller_workflow():
-    client = await Client.connect("localhost:7233", namespace="my-caller-namespace")
-    task_queue = "my-caller-task-queue"
-
-    async with Worker(
-        client,
-        task_queue=task_queue,
-        workflows=[Echo2CallerWorkflow],
-        workflow_runner=UnsandboxedWorkflowRunner(),
-    ):
-        print("🟠 Caller worker started")
-        result = await client.execute_workflow(
-            Echo2CallerWorkflow.run,
-            "hello",
-            id="my-caller-workflow-id",
-            task_queue=task_queue,
-        )
-        print("🟢 workflow result:", result)
-
-
-async def execute_echo3_caller_workflow():
-    client = await Client.connect("localhost:7233", namespace="my-caller-namespace")
-    task_queue = "my-caller-task-queue"
-
-    async with Worker(
-        client,
-        task_queue=task_queue,
-        workflows=[Echo3CallerWorkflow],
-        workflow_runner=UnsandboxedWorkflowRunner(),
-    ):
-        print("🟠 Caller worker started")
-        result = await client.execute_workflow(
-            Echo3CallerWorkflow.run,
-            "hello",
-            id="my-caller-workflow-id",
-            task_queue=task_queue,
-        )
-        print("🟢 workflow result:", result)
-
-
-async def execute_hello_caller_workflow():
-    client = await Client.connect("localhost:7233", namespace="my-caller-namespace")
-    task_queue = "my-caller-task-queue"
-
-    async with Worker(
-        client,
-        task_queue=task_queue,
-        workflows=[HelloCallerWorkflow],
-        workflow_runner=UnsandboxedWorkflowRunner(),
-    ):
-        print("🟠 Caller worker started")
-        result = await client.execute_workflow(
-            HelloCallerWorkflow.run,
-            "world",
-            id="my-caller-workflow-id",
-            task_queue=task_queue,
-        )
-        print("🟢 workflow result:", result)
-
-
-async def execute_hello2_caller_workflow():
-    client = await Client.connect("localhost:7233", namespace="my-caller-namespace")
-    task_queue = "my-caller-task-queue"
-
-    async with Worker(
-        client,
-        task_queue=task_queue,
-        workflows=[Hello2CallerWorkflow],
-        workflow_runner=UnsandboxedWorkflowRunner(),
-    ):
-        print("🟠 Caller worker started")
-        result = await client.execute_workflow(
-            Hello2CallerWorkflow.run,
-            "world",
+            workflow_cls.run,
+            input,
             id="my-caller-workflow-id",
             task_queue=task_queue,
         )
@@ -121,17 +42,17 @@ if __name__ == "__main__":
         sys.exit(1)
 
     [wf_name] = sys.argv[1:]
-    fn = {
-        "hello": execute_hello_caller_workflow,
-        "hello2": execute_hello2_caller_workflow,
-        "echo": execute_echo_caller_workflow,
-        "echo2": execute_echo2_caller_workflow,
-        "echo3": execute_echo3_caller_workflow,
+    coro = {
+        "echo": execute_workflow(EchoCallerWorkflow, "hello"),
+        "echo2": execute_workflow(Echo2CallerWorkflow, "hello"),
+        "echo3": execute_workflow(Echo3CallerWorkflow, "hello"),
+        "hello": execute_workflow(HelloCallerWorkflow, "world"),
+        "hello2": execute_workflow(Hello2CallerWorkflow, "world"),
     }[wf_name]
 
     loop = asyncio.new_event_loop()
     try:
-        loop.run_until_complete(fn())
+        loop.run_until_complete(coro)
     except KeyboardInterrupt:
         interrupt_event.set()
         loop.run_until_complete(loop.shutdown_asyncgens())
