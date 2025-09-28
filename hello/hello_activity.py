aef2f31a2170028cf64b9efdf8caa02c56be03b5
Dan Davison <dan.davison@temporal.io>
2025-09-28 05:34:57 -0400

    wip

--- hello/hello_activity.py
+++ hello/hello_activity.py
@@ -1,6 +1,4 @@
 import asyncio
-from concurrent.futures import ThreadPoolExecutor
-from dataclasses import dataclass
 from datetime import timedelta
 
 from temporalio import activity, workflow
@@ -8,65 +6,43 @@ from temporalio.client import Client
 from temporalio.worker import Worker
 
 
-# While we could use multiple parameters in the activity, Temporal strongly
-# encourages using a single dataclass instead which can have fields added to it
-# in a backwards-compatible way.
-@dataclass
-class ComposeGreetingInput:
-    greeting: str
-    name: str
-
-
-# Basic activity that logs and does string concatenation
 @activity.defn
-def compose_greeting(input: ComposeGreetingInput) -> str:
-    activity.logger.info("Running activity with parameter %s" % input)
-    return f"{input.greeting}, {input.name}!"
+async def coin_flip() -> float:
+    import random
+
+    return random.random()
 
 
-# Basic workflow that logs and invokes an activity
 @workflow.defn
-class GreetingWorkflow:
+class RandomNumberOfAttemptsWorkflow:
     @workflow.run
-    async def run(self, name: str) -> str:
-        workflow.logger.info("Running workflow with parameter %s" % name)
-        return await workflow.execute_activity(
-            compose_greeting,
-            ComposeGreetingInput("Hello", name),
-            start_to_close_timeout=timedelta(seconds=10),
-        )
+    async def run(self) -> int:
+        attempts = 0
+        while True:
+            attempts += 1
+            random_number = await workflow.execute_activity(
+                coin_flip,
+                start_to_close_timeout=timedelta(seconds=10),
+            )
+            if random_number < 0.5:
+                return attempts
 
 
 async def main():
-    # Uncomment the lines below to see logging output
-    # import logging
-    # logging.basicConfig(level=logging.INFO)
-
-    # Start client
     client = await Client.connect("localhost:7233")
-
-    # Run a worker for the workflow
     async with Worker(
         client,
-        task_queue="hello-activity-task-queue",
-        workflows=[GreetingWorkflow],
-        activities=[compose_greeting],
-        # Non-async activities require an executor;
-        # a thread pool executor is recommended.
-        # This same thread pool could be passed to multiple workers if desired.
-        activity_executor=ThreadPoolExecutor(5),
+        task_queue="coin-flip-task-queue",
+        workflows=[RandomNumberOfAttemptsWorkflow],
+        activities=[coin_flip],
     ):
-
-        # While the worker is running, use the client to run the workflow and
-        # print out its result. Note, in many production setups, the client
-        # would be in a completely separate process from the worker.
-        result = await client.execute_workflow(
-            GreetingWorkflow.run,
-            "World",
-            id="hello-activity-workflow-id",
-            task_queue="hello-activity-task-queue",
+        print(
+            await client.execute_workflow(
+                RandomNumberOfAttemptsWorkflow.run,
+                id="coin-flip-workflow-id",
+                task_queue="coin-flip-task-queue",
+            )
         )
-        print(f"Result: {result}")
 
 
 if __name__ == "__main__":
