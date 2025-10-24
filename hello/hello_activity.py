3d1408f22acbbb42bbd63827ba3dff72b410ba9c
Dan Davison <dan.davison@temporal.io>
2025-10-24 10:29:07 -0400

    Revert "Evolve into Standalone Activity"
    This reverts commit bb21f038908b8e56daef163f67a2325fb248b203.

--- hello/hello_activity.py
+++ hello/hello_activity.py
@@ -1,7 +1,7 @@
 import asyncio
 from datetime import timedelta
 
-from temporalio import activity
+from temporalio import activity, workflow
 from temporalio.client import Client
 from temporalio.worker import Worker
 
@@ -12,13 +12,22 @@ async def my_activity(input: int) -> int:
     return input + 1
 
 
+@workflow.defn
+class MyWorkflow:
+    @workflow.run
+    async def run(self) -> int:
+        return await workflow.execute_activity(
+            my_activity,
+            1,
+            start_to_close_timeout=timedelta(seconds=10),
+        )
+
+
 async def my_client_code(client: Client):
-    result = await client.execute_activity(
-        my_activity,
-        1,
-        id="my-entity-id",
+    result = await client.execute_workflow(
+        MyWorkflow.run,
+        id="wid",
         task_queue="tq",
-        start_to_close_timeout=timedelta(seconds=10),
     )
     print(f"Activity result: {result}")
 
@@ -28,6 +37,7 @@ async def main():
     async with Worker(
         client,
         task_queue="tq",
+        workflows=[MyWorkflow],
         activities=[my_activity],
         max_concurrent_activity_task_polls=2,
         max_concurrent_workflow_task_polls=2,
