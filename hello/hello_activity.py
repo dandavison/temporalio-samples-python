591cdd0141fad6cb5e92a428cca12b0b60865ba1
Dan Davison <dandavison7@gmail.com>
2025-12-19 14:56:09 -0500

    Standalone activity

--- hello/hello_activity.py
+++ hello/hello_activity.py
@@ -1,7 +1,7 @@
 import asyncio
 from datetime import timedelta
 
-from temporalio import activity, workflow
+from temporalio import activity
 from temporalio.client import Client
 from temporalio.worker import Worker
 
@@ -12,22 +12,13 @@ async def my_activity(input: int) -> int:
     return input + 1
 
 
-@workflow.defn
-class MyWorkflow:
-    @workflow.run
-    async def run(self) -> int:
-        return await workflow.execute_activity(
-            my_activity,
-            1,
-            start_to_close_timeout=timedelta(seconds=10),
-        )
-
-
 async def my_client_code(client: Client):
-    result = await client.execute_workflow(
-        MyWorkflow.run,
-        id="wid",
+    result = await client.execute_activity(
+        my_activity,
+        1,
+        id="my-activity-id",
         task_queue="tq",
+        start_to_close_timeout=timedelta(seconds=10),
     )
     print(f"Activity result: {result}")
 
@@ -37,7 +28,6 @@ async def main():
     async with Worker(
         client,
         task_queue="tq",
-        workflows=[MyWorkflow],
         activities=[my_activity],
         max_concurrent_activity_task_polls=2,
         max_concurrent_workflow_task_polls=2,
